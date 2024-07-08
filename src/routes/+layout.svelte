93a9e66e390b91cdc01c3089fb25a599031b071b
MajorTimberWolf <ak0929@srmist.edu.in>
2024-07-09 00:48:29 +0530

    chore: update package and dependency
    Breaking Change: update from json to yaml

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import './styles.css';
+</script>
+
+<svelte:head>
+	<title>Abhinav Rajeev Kumar</title>
+	<meta name="description" content="Personal Portfolio" />
+	<meta name="keywords" content="Abhinav Rajeev Kumar, Portfolio, Personal Portfolio" />
+	<meta name="author" content="Abhinav Rajeev Kumar" />
+	<meta name="robots" content="index, follow" />
+	<meta name="theme-color" content="#000000" />
+	<meta name="msapplication-TileColor" content="#000000" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+</svelte:head>
+
+<slot />
